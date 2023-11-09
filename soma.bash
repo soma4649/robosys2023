fae9c7ff08c311e8445268bfcc5ffe69a6976cf9
Soma <vxjp4649takeru@icloud.com>
2023-11-09 11:22:25 +0900

    add

--- soma.bash
+++ soma.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Takeru Soma
 # SPDX-License-Identifier: BSd-3-Clause
 
@@ -9,9 +9,18 @@ ng () {
 
 ret=0
 
-### I/O TEST ###
+### I/O ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$ret" = 0 ] && echo OK
 exit $ret 
