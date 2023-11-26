99bb6e18df4801beb634ddec7885345f66744cbd
Takeru Soma <vxjp4649tkeru@icloud.com>
2023-11-27 03:06:31 +0900

    update test2.bash

--- test2.bash
+++ test2.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/bash  
+#!/usr/bin/bash -xv 
 # SPDX-FileCopyrightText: 2023 Takeru Soma
 # SPDX-License-Identifier: BSD-3-clause
 
@@ -12,6 +12,20 @@ ret=0
 out=$(echo 千葉県 | ./prefectures)
 
 [ "${out}" = 都道府県名を入力してください：千葉 ] || ng ${LINENO}
+
+### 間違えた入力　###
+
+out=$(echo あ | ./prefectures)
+[ "$?" = 0 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo a | ./prefectures) 
+[ "$?" = 0 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo  | ./prefectures)
+[ "$?" = 0 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "$ret" = 0 ] && echo OK
 exit $ret
